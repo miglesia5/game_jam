3245fb1cb3a1fe41a8a4bef4bd822a89a60202d0
miglesia5 <manuiglesias55@gmail.com>
2020-07-24 15:06:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function random(min, max) {
 
 function setBG() {
 	if (Math.round(Math.random())) {
-		return "https://eimzpink.files.wordpress.com/2015/07/simpsons-donut.png";
+		return "https://www.cookingclassy.com/wp-content/uploads/2014/06/chocolate-chip-cookie-16.jpg";
 	} else {
 		return "http://icons.iconarchive.com/icons/gordon-irving/simpsons-2/128/Duff-2-icon.png";
 	}
@@ -30,7 +30,7 @@ function dropBox() {
 	thisBox.data("test", Math.round(Math.random()));
 	if (thisBox.data("test")) {
 		thisBox.css({
-			"background": "url('https://eimzpink.files.wordpress.com/2015/07/simpsons-donut.png')",
+			"background": "https://www.cookingclassy.com/wp-content/uploads/2014/06/chocolate-chip-cookie-16.jpg')",
 			"background-size": "contain"
 		});
 	} else {
